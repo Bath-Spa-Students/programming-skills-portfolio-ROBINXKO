219a3b935680c4c4290015d343d2df418465af5b
ROBINXKO <robinxoko@gmail.com>
2023-10-24 10:24:37 +0400

    Chp2-exercise3.py
    Print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,13 @@
+name = "\tRobin Hood\n"
+
+print("original:")
+print(name)
+
+print("Using lstrip():")
+print(name.lstrip())
+
+print("Using rstrip():")
+print(name.rstrip())
+
+print("\nUsing strip():")
+print(name.strip())
