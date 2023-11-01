ce1070e240e79a9c03f71ed9d31e134823b3b33b
ROBINXKO <robinxoko@gmail.com>
2023-11-02 01:43:44 +0400

    Chp3-exercise2
    Personalized massage for each friend using 'for loop'.

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,8 @@
+#List of friends names
+friends=['Daniel','James','Tyron','Stephen']
+
+#For loop
+for friend in friends:
+    
+#Personalized massage for the friends
+    print(f'Hello {friend}, I would like to invite you for dinner this friday, at my house.')
