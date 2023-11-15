50c36ebbf78229d7ae38fdc2b402a3d297c24667
ROBINXKO <robinxoko@gmail.com>
2023-11-16 00:11:22 +0400

    Chp3-Exercise3
    Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
    
    to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,6 @@
+#list of  mode of tranportation
+modes_of_transport={'horse','camel','ford GT','private jet','porsche 911'}
+
+#Statement about those modes of transport
+for mode in modes_of_transport:
+    print(f"I would like to own a {mode}.")
