efa87b3a56efa892b8a9770cdfda496a6734cb6c
ROBINXKO <robinxoko@gmail.com>
2023-11-16 00:12:57 +0400

    Chp3-Exercise4
    If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,6 @@
+#List of guests
+great_minds={'albert einstein','nikola tesla','isaac newton'}
+
+#Message inviting them for dinner
+for guest in great_minds:
+    print(f"Sir {guest}, it gives me great pleasure to invite you to dinner at my residence. I look forward for a positive reponse from you.")
