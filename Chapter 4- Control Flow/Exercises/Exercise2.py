b616f2b9b9c83975dfa12adc77b69daf02d8c71a
ROBINXKO <robinxoko@gmail.com>
2023-11-18 16:09:41 +0400

    Chapter4-Exercise2

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,15 @@
+# Program that passes the if test
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien!")
+else:
+    print("You just earned 10 points!")
+
+# Program that fails the if test
+alien_color = 'red'
+
+if alien_color == 'yellow':
+    print("You just earned 5 points for shooting the alien!")
+else:
+    print("You just earned 10 points!")
